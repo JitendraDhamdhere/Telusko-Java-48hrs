2d8626b1e8d20cf30ab0a7db689ac663737b474f
jitendrad <jitendrad@artcode.in>
2025-06-19 18:29:24 +0530

    Variables, DataType ,comments

---
+++ corejava/DataTypes.java
@@ -0,0 +1,29 @@
+package corejava;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        //Comments
+
+        //Single-line comments
+
+        /*
+          Multiline comments
+          more than one line ---Compiler ignore the data in comment section
+         */
+
+ /*
+         *   Documentation comments 
+         *    
+         *
+         */
+        //Variables
+        // data type variable [ = value][, variable [ = value] ...] ;
+        int a, b, c;         // Declares three ints, a, b, and c.
+        //int a = 10, b = 10;  // Example of initialization
+        byte B = 22;         // initializes a byte type variable B.
+        double pi = 3.14159; // declares and assigns a value of PI.
+        //char a = 'a';        // the char variable a iis initialized with value 'a'
+
+    }
+}
